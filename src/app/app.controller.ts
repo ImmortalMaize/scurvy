c9a907ed55372dc645b2ee794dc2772dc3c2606f
Maize~ <the.scarlet.armada@gmail.com>
2023-08-11 20:49:27 -0400

    oh yeah

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -5,7 +5,11 @@ import { DatabaseService } from '../database/database.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService, private readonly databaseService: DatabaseService) {}
-
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  
   @Delete()
   async clear(@Body('model') model: string) {
     return await this.databaseService.clear(model)
